5eeddc54421dddddacf940e024fb12641389785d
Michael BAUDIN <michael.baudin@gmail.com>
2024-12-26 23:37:36 +0100

    openturns_utils: Added comments

--- modules/openturns_utils.py
+++ modules/openturns_utils.py
@@ -1,5 +1,6 @@
 import openturns as ot
 
+
 def get_ot_distribution(problem):
     """
     Return the OpenTURNS model
@@ -8,43 +9,46 @@ def get_ot_distribution(problem):
     ----------
     problem : the problem
         The probabilistic problem
-    
+
     Returns
     -------
     distribution : ot.Distribution
         The probabilistic model
     """
     marginals = []
-    for dist_info in problem['distributions']:
-        dist_type = dist_info['type']
-        params = dist_info['params']
-        if dist_type == 'Uniform':
+    for dist_info in problem["distributions"]:
+        dist_type = dist_info["type"]
+        params = dist_info["params"]
+        if dist_type == "Uniform":
             a, b = params
             marginals.append(ot.Uniform(a, b))
-        elif dist_type == 'Normal':
+        elif dist_type == "Normal":
             mu, sigma = params
             marginals.append(ot.Normal(mu, sigma))
-        elif dist_type == 'Gumbel':
+        elif dist_type == "Gumbel":
             beta_param, gamma_param = params
             marginals.append(ot.Gumbel(beta_param, gamma_param))
-        elif dist_type == 'Triangular':
+        elif dist_type == "Triangular":
             a, m, b = params
             marginals.append(ot.Triangular(a, m, b))
-        elif dist_type == 'Beta':
+        elif dist_type == "Beta":
             alpha, beta_value, a, b = params
             marginals.append(ot.Beta(alpha, beta_value, a, b))
-        elif dist_type == 'LogNormal':
+        elif dist_type == "LogNormal":
             mu, sigma, gamma = params
             marginals.append(ot.LogNormal(mu, sigma, gamma))
-        elif dist_type == 'LogNormalMuSigma':
+        elif dist_type == "LogNormalMuSigma":
             mu, sigma, gamma = params
-            marginals.append(ot.ParametrizedDistribution(ot.LogNormalMuSigma(mu, sigma, gamma)))
+            marginals.append(
+                ot.ParametrizedDistribution(ot.LogNormalMuSigma(mu, sigma, gamma))
+            )
         else:
             raise ValueError(f"Unsupported distribution type: {dist_type}")
-        
+
     distribution = ot.ComposedDistribution(marginals)
-    distribution.setDescription(problem['names'])
+    distribution.setDescription(problem["names"])
     return distribution
+
 
 def get_ot_model(model, problem):
     """
@@ -56,15 +60,29 @@ def get_ot_model(model, problem):
         The physical model
     problem : the problem
         The probabilistic problem
-    
+
     Returns
     -------
     physical_model : ot.Function
         The physical model g
     """
-    ot_model = ot.PythonFunction(problem['num_vars'], 1, model)
+    ot_model = ot.PythonFunction(problem["num_vars"], 1, model)
     return ot_model
 
+
 def ot_point_to_list(point):
+    """
+    Return a list corresponding to the OpenTURNS point
+
+    Parameters
+    ----------
+    point : ot.Point
+        The point
+
+    Returns
+    -------
+    values : list(float)
+        The point
+    """
     dimension = point.getDimension()
     return [point[i] for i in range(dimension)]
